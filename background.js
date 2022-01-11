8a41088a6a61e285d74623e201430465a75dfd72
Johan Lakshmanan <61603601+JTRON780@users.noreply.github.com>
2022-01-11 16:43:50 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    // Send a message to the active tab
+    // Active Tab Activator
     chrome.tabs.query({
         active: true,
         currentWindow: true
